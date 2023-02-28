f9e250e7e0c6cd1c10b08cfe3138866a8aac5b66
Akshay Panchal <akshay.panchalhp@gmail.com>
2023-02-28 03:29:32 -0500

    commit_2

--- Netflix/netflix/src/components/Home/Header/header.jsx
+++ Netflix/netflix/src/components/Home/Header/header.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import logo from "../../../assets/logo.png"
+import {ImSearch} from "react-icons/im";
+
 const header = () => {
   return (
     <nav className="header">
 
     <img src={logo} alt="logo" />
 
+    <div>
+      <Link to={"/tvshows"}>TV shows</Link>
+      <Link to={"/movies"}>Movies</Link>
+      <Link to={"/recent"}>Recently Added</Link>
+      <Link to={"/mylist"}>My List</Link>
+    </div>
+
+    <ImSearch />
     </nav>
   )
 }
