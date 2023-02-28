e994e1579bf53ca4facd4a065b06de42c4b56d12
Akshay Panchal <akshay.panchalhp@gmail.com>
2023-02-28 01:17:15 -0500

    first commit

---
+++ Netflix/netflix/src/components/Home/Header/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from "../../../assets/logo.png"
+const header = () => {
+  return (
+    <nav className="header">
+
+    <img src={logo} alt="logo" />
+
+    </nav>
+  )
+}
+
+export default header
